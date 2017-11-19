8766c22ee60fa8f592dfd219390e4b1d3254c11c
Hamadoz <ghemid_m_dz@yahoo.fr>
2017-11-19 17:22:33 +0100

    production code

--- app/src/main/java/com/example/ghemid/gesture_detector/MainActivity.java
+++ app/src/main/java/com/example/ghemid/gesture_detector/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.ghemid.gesture_detector;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,42 +8,26 @@ import android.hardware.SensorManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     private SensorManager mSensorManager;
     private Sensor mProximity;
-    private EditText editTextProximityValue, editTextMaxRange, editTextAction;
-    private Date date, stayDate;
+    private Date date;
     private int startTime = 0, endTime = 0, numberOfSlidesPerSecond = 0;
-    private long startTimeStay, endTimeStay;
-    private static boolean firstTest = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextProximityValue = (EditText) findViewById(R.id.editTextProximityValue);
-        editTextMaxRange = (EditText) findViewById(R.id.editTextMaxRage);
-        editTextAction = (EditText) findViewById(R.id.editTextAction);
-
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-
-        if (mProximity == null) {
-            editTextProximityValue.setText("No Proximity Sensor!");
-        } else {
-            editTextProximityValue.setText(mProximity.getName());
-            editTextMaxRange.setText(String.valueOf(mProximity.getMaximumRange()));
+        if (mProximity != null) {
             mSensorManager.registerListener(proximitySensorEventListener,
                     mProximity,
                     SensorManager.SENSOR_DELAY_NORMAL
@@ -69,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
             // TODO Auto-generated method stub
 
             if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
-                editTextProximityValue.setText(String.valueOf(event.values[0]));
 
                 if (event.values[0] == 0.0) {
                     date = new Date();
@@ -90,40 +71,34 @@ public class MainActivity extends AppCompatActivity {
                             date = new Date();
                             endTime = date.getSeconds();
 
-                            stayDate = new Date();
-                            endTimeStay = System.currentTimeMillis() % 1000;
 
                             if (endTime > startTime + 2) {
                                 // ====== Validate action ======
-                                editTextAction.setText("Validate action");
-                                Handler handler = new Handler();
-                                handler.postDelayed(new Runnable() {
-                                    public void run() {
-                                        editTextAction.setText("");
-                                    }
-                                }, 1500);
-
+                                /*
+                                -
+                                -
+                                - PUT YOUR CODE HERE
+                                -
+                                -
+                                 */
                             } else if (numberOfSlidesPerSecond > 1) {
                                 // ====== Slice twice action =======
-                                editTextAction.setText("Slide twice action");
-                                Handler handler = new Handler();
-                                handler.postDelayed(new Runnable() {
-                                    public void run() {
-                                        editTextAction.setText("");
-                                    }
-                                }, 1500);
-
+                                /*
+                                -
+                                -
+                                - PUT YOUR CODE HERE
+                                -
+                                -
+                                 */
                             } else {
-
                                 // ====== Slice once action =======
-                                editTextAction.setText("Slide once action");
-                                Handler handler = new Handler();
-                                handler.postDelayed(new Runnable() {
-                                    public void run() {
-                                        editTextAction.setText("");
-                                    }
-                                }, 1500);
-
+                                /*
+                                -
+                                -
+                                - PUT YOUR CODE HERE
+                                -
+                                -
+                                 */
                             }
                         }
                     }, 500);
